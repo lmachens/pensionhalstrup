dd2d3eb51662f84d2dd8b0e5b8d97f75fcb2cd4d
lmachens <leon.machens@googlemail.com>
2019-12-12 12:31:29 +0100

    Add layout
    closes #17

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,32 @@
 import React from 'react';
+import GlobalStyles from './GlobalStyles';
+import { ThemeProvider } from 'emotion-theming';
+import light from './themes/light';
 import Logo from './components/Logo';
+import styled from '@emotion/styled';
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  max-width: 1000px;
+  margin: 0 auto;
+`;
+
+const Header = styled.header`
+  text-align: center;
+`;
 
 function App() {
   return (
-    <div>
-      <header>
-        <Logo />
-      </header>
-    </div>
+    <ThemeProvider theme={light}>
+      <GlobalStyles />
+      <Container>
+        <Header>
+          <Logo />
+        </Header>
+        <main></main>
+      </Container>
+    </ThemeProvider>
   );
 }
 
