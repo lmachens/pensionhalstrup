57fe3f2d72da45233515e556edb9f0f2b2622725
Leon Machens <leon.machens@googlemail.com>
2019-12-15 10:43:07 +0100

    Add map

---
+++ client/src/components/Map.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Map() {
+  return (
+    <iframe
+      src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2386.4407622712424!2d7.911440615974237!3d53.26372188859773!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47b6fae4acf35869%3A0x8c92595791d3fc5!2sPension%20Halstrup!5e0!3m2!1sde!2sde!4v1576402747123!5m2!1sde!2sde"
+      width="600"
+      height="450"
+      frameborder="0"
+      style={{ border: 0 }}
+      allowfullscreen=""
+    />
+  );
+}
+
+export default Map;
