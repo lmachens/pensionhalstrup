5d7a5867711aac237fda7a573fad6b4b5affa1e4
Leon Machens <leon.machens@googlemail.com>
2019-12-15 10:36:24 +0100

    Add contact informations
    closes #4

---
+++ client/src/components/Contact.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+function Contact() {
+  return (
+    <>
+      <h3>Ansprechpartner</h3>
+      <p>
+        Esther Welter
+        <br />
+        Alte Straße 32
+        <br />
+        26655 Westerstede
+      </p>
+      <h3>Telefon</h3>
+      <p>
+        04488 / 2318
+        <br />
+        0162 / 9128599
+        <br />
+        Kontakt über <a href="mailto:esther.welter78@ewetel.net">e-Mail</a>
+      </p>
+    </>
+  );
+}
+
+export default Contact;
