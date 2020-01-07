f34f3d62221f9119d311dc389409ebf33d7df806
Leon Machens <leon.machens@googlemail.com>
2020-01-07 10:10:53 +0100

    Server App and Storybook from server (#35)
    * Server App and Storybook from server
    
    * Fix now routes

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const express = require('express');
+const path = require('path');
 const { initDb } = require('./lib/database');
 const { getImages } = require('./lib/images');
 
@@ -8,6 +9,14 @@ const app = express();
 app.get('/api/images', async (request, response) => {
   const images = await getImages();
   response.json(images);
+});
+
+// Serve any static files
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+// Handle React routing, return all requests to React app
+app.get('*', function(req, res) {
+  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
 });
 
 initDb(process.env.MONGO_URL, process.env.DB_NAME).then(() => {
